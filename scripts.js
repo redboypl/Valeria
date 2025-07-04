9349ce1749491d7fd7cbab758ed587bb3c86f084
redboypl <135893539+redboypl@users.noreply.github.com>
2025-07-04 00:29:53 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Contador regresivo
+const countdownEl = document.getElementById("countdown");
+const targetDate = new Date("2025-09-20T00:00:00");
+
+function updateCountdown() {
+  const now = new Date();
+  const diff = targetDate - now;
+
+  if (diff <= 0) {
+    countdownEl.innerHTML = "¡Es hoy!";
+    return;
+  }
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  countdownEl.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+// Función para cerrar el popup
+function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+  }
+// Mostrar popup al cargar la página
+window.onload = function() {
+  updateCountdown();
+  setInterval(updateCountdown, 1000);
+  document.getElementById('popup').style.display = 'flex';
+}
